5e39a3b087ec1490143dad7398b3cb327729c30e
Harsh <harsh.cr7@gmail.com>
2025-02-16 11:59:12 +0530

    Changed the fonts and gradient

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 
-const  about= () => {
+const  About= () => {
     return (  
-        <div className="w-screen bg-blue-900 m-0 h-[655px] relative">
-            <div className=" text-white text-6xl font-serif p-5 absolute left-80 top-1 font-bold">
+        <div className="w-screen bg-gradient-custom m-0 h-[655px] relative">
+            <div className=" text-white text-6xl font-playfair p-5 absolute left-80 top-1 font-bold">
                 ABOUT US
             </div>
             <div>
-                <div className="text-white font-black text-4xl p-8 w-[900px] absolute top-24">
-                    
-                    our Vision <br></br>
-                    is BOLD <br></br>
-                    and our resolve <br></br>
-                    stronger then ever<br></br>
-                     
+            <div className="text-white font-titan text-5xl p-8 w-[900px] absolute top-24">
+                    <span className="uppercase text-6xl font-extrabold tracking-wide">
+                        our vision
+                    </span> <br />
+                    <span className="italic font-water text-5xl">
+                        is bold
+                    </span> <br />
+                    <span className="uppercase text-6xl font-extrabold tracking-wide">
+                        our resolve
+                    </span> <br />
+                    <span className="italic font-water text-5xl">
+                        is stronger than ever
+                    </span> <br />
                 </div>
-                <div className="text-white text-xl absolute bottom-4 font-serif p-10 flex justify-center items-center w-[900px]">
+
+                <div className="text-white text-xl absolute bottom-4 font-playfair p-10 flex justify-center items-center w-[900px]">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur natus illum rerum praesentium voluptatem voluptates quasi veniam, odio facilis placeat accusamus harum fuga, at corrupti exercitationem inventore itaque velit hic quod autem. Veniam illum consequuntur aperiam quis necessitatibus. Deleniti doloribus neque minima provident! Rerum, quae! Illo, odio rerum qui aliquid rem ea? Rerum laboriosam fugit delectus iusto quidem libero cupiditate minima perferendis perspiciatis officiis adipisci quo saepe maiores doloribus facere id atque aperiam est autem sed pariatur expedita ab, placeat quisquam? Fugiat odio quod, odit temporibus, atque unde molestias ducimus quam optio consequatur blanditiis, numquam adipisci? Itaque quod possimus reiciendis.
                 </div>
                 <div className="absolute right-0 top-0">
@@ -29,4 +36,4 @@ const  about= () => {
     );
 }
  
-export default about;
+export default About;
