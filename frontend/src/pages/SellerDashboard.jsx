1e11bea2bbc4bc651c561cefc8ee093d33d2791b
Yash Pratap Singh Deora <yashpratapsinghdeora1@gmail.com>
2025-03-19 01:18:03 +0530

    user dash and seller dash partially done

---
+++ frontend/src/pages/SellerDashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SideBarProfile from "../components/SideBarProfile";
+import SellerMainDashSection from "../components/SellerMainDashSection";
+
+const  SellerDashboard= () => {
+    return (  
+        <div className="mt-[6vh] h-[94vh] flex justify-between">
+            <SideBarProfile/>
+            <SellerMainDashSection />
+        </div>
+    );
+}
+ 
+export default SellerDashboard;
