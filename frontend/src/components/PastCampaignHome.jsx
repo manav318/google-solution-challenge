013c3e3cd65107b1c355dbc7d931efb9b9f4cc9d
Yash Pratap Singh Deora <yashpratapsinghdeora1@gmail.com>
2025-03-25 00:03:56 +0530

    pastcamp responsiveness resolved part1

--- frontend/src/components/PastCampaignHome.jsx
+++ frontend/src/components/PastCampaignHome.jsx
@@ -3,28 +3,28 @@ import { Link } from "react-router-dom";
 
 const Past = () => {
     return (
-        <div className="min-h-[1200px] w-[88%] border-0 mx-auto p-10 shadow-[0_20px_50px_rgba(0,0,0,0.5)]">
-            <h2 className="text-6xl font-serif text-blue-600 mb-24 text-center">
+        <div className="h-[165vh] w-[88vw] border-0 mx-auto p-10 shadow-[0_5vh_7.5vh_rgba(0,0,0,0.5)]">
+            <h2 className="text-4xl md:text-5xl lg:text-6xl font-serif text-blue-600 mb-[10vh] text-center">
                 What Our Community <br /> Has Been Up To.
             </h2>
-            <div className="relative min-h-[900px]">
-                <div className="w-[3px] h-[760px] bg-black absolute left-[50%] translate-x-[-50%] top-0"></div>
+            <div className="relative h-[125vh]">
+                <div className="w-[3px] h-[105vh] bg-black absolute left-[50%] translate-x-[-50%] top-0"></div>
                 <div className="text-5xl text-black font-bold absolute left-[50%] translate-x-[-50%] top-16">~</div>
                 <div className="text-5xl text-black font-bold absolute left-[50%] translate-x-[-50%] top-80">~</div>
                 <div className="text-5xl text-black font-bold absolute left-[50%] translate-x-[-50%] top-[620px]">~</div>
-                <div className="w-[40%] absolute top-12 left-[5%]">
+                <div className="w-[40%] absolute top-[5vh] left-[5%]">
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi recusandae aliquid labore? Nisi eligendi dolores modi explicabo. Quas totam, nihil ea adipisci optio quia quasi magni cumque! Suscipit at dolore veritatis optio illo dolorum nihil accusantium amet! Natus debitis commodi aut explicabo earum id incidunt libero amet repudiandae, cumque odit?
                 </div>
-                <div className="w-[40%] absolute top-[280px] right-[5%]">
+                <div className="w-[40%] absolute top-[39vh] right-[5%]">
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi recusandae aliquid labore? Nisi eligendi dolores modi explicabo. Quas totam, nihil ea adipisci optio quia quasi magni cumque! Suscipit at dolore veritatis optio illo dolorum nihil accusantium amet! Natus debitis commodi aut explicabo earum id incidunt libero amet repudiandae, cumque odit?
                 </div>
-                <div className="w-[40%] absolute top-[590px] left-[5%]">
+                <div className="w-[40%] absolute top-[84vh] left-[5%]">
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi recusandae aliquid labore? Nisi eligendi dolores modi explicabo. Quas totam, nihil ea adipisci optio quia quasi magni cumque! Suscipit at dolore veritatis optio illo dolorum nihil accusantium amet! Natus debitis commodi aut explicabo earum id incidunt libero amet repudiandae, cumque odit?
                 </div>
-                <div className="absolute top-0 right-[15%] flex justify-center items-center h-[200px] bg-transparent">
+                <div className="absolute top-0 right-[15%] flex justify-center items-center h-[25vh] bg-transparent">
                     {/* Video Container */}
                     <div
-                        className="w-[300px] h-[200px] overflow-hidden shadow-2xl"
+                        className="w-[20vw] h-[25vh] overflow-hidden shadow-2xl"
                         style={{
                             clipPath: "path('M50 0 Q200 50 350 0 Q500 -50 650 0 L650 300 L50 300 Z')"
                         }}
@@ -34,10 +34,10 @@ const Past = () => {
                         </video>
                     </div>
                 </div>
-                <div className="absolute top-[280px] left-[15%] flex justify-center items-center h-[200px] bg-transparent">
+                <div className="absolute top-[36vh] left-[15%] flex justify-center items-center h-[25vh] bg-transparent">
                     {/* Video Container */}
                     <div
-                        className="w-[300px] h-[200px] overflow-hidden shadow-2xl"
+                        className="w-[20vw] h-[25vh] overflow-hidden shadow-2xl"
                         style={{
                             clipPath: "path('M50 50 C150 -30, 250 120, 350 50 C450 -20, 550 150, 650 50 L650 300 L50 300 Z')"
                         }}
@@ -47,10 +47,10 @@ const Past = () => {
                         </video>
                     </div>
                 </div>
-                <div className="absolute top-[590px] right-[15%] flex justify-center items-center h-[200px] bg-transparent">
+                <div className="absolute top-[80vh] right-[15%] flex justify-center items-center h-[25vh] bg-transparent">
                     {/* Video Container */}
                     <div
-                        className="w-[300px] h-[200px] overflow-hidden shadow-2xl"
+                        className="w-[20vw] h-[25vh] overflow-hidden shadow-2xl"
                         style={{
                             clipPath: "path('M50 0 Q200 50 350 0 Q500 -50 650 0 L650 300 L50 300 Z')"
                         }}
