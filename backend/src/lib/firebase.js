f46e52a0bcf8e8911d8cad6a5e05e445dcec360e
Manav Ekka <itsmanav999@gmail.com>
2025-03-26 23:10:34 +0530

    Update firebase.js

--- backend/src/lib/firebase.js
+++ backend/src/lib/firebase.js
@@ -1,5 +1,5 @@
 const admin=require("firebase-admin")
-const credentials=require("../firebaseService.json")
+const credentials=require(".../firebaseService.json")
 
 admin.initializeApp({
     credential:admin.credential.cert(credentials),
